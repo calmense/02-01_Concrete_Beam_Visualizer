bbec9c829e0423bed8a7f33fce3b8cc61259edfa
Cal Mense <104301991+calmense@users.noreply.github.com>
2024-03-05 16:39:30 +0100

    Update concrete_beam_visualizer_main.py

--- concrete_beam_visualizer_main.py
+++ concrete_beam_visualizer_main.py
@@ -19,7 +19,10 @@ st.set_page_config(page_title="Concrete Beam Visualizer", layout="wide")
 st.markdown("""<style>
 [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 500px;}
 [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 500px;margin-left: -500px;}
-</style>""",unsafe_allow_html=True)
+</style>;
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>""",unsafe_allow_html=True)
 
 st.markdown('''
 <style>
